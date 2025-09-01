82ce0c461b9eb6d029159117be196bdb2ebe2aa2
VictorAPuntel <victor.a.puntel@gmail.com>
2025-09-01 16:04:06 -0300

    Creating project for introduction for react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css'
+import { Button } from './components/button'
+import styles from './app.module.css'
+
+export function App() {
+  return (
+    <div className={styles.container}>
+      <Button name="Adicionar" />
+      <span>0</span>
+      <Button name="Remover" />
+    </div>
+  )
+}

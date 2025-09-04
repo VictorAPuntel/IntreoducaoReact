e964d7480e86670ebbe64b7371ff839bf300700e
VictorAPuntel <victor.a.puntel@gmail.com>
2025-09-03 21:29:05 -0300

    using styles and useState e useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import './global.css'
+import { useState, useEffect } from 'react'
 import { Button } from './components/button'
 import styles from './app.module.css'
+// import { useMessage } from './hooks/useMessage'
 
 export function App() {
+  const [count, setCount] = useState(0)
+  // const message = useMessage({ name: 'Victor', age: 28 })
+
+  function handleAdd() {
+    setCount(count + 1)
+  }
+
+  function handleRemove() {
+    if (count > 0) {
+      setCount(count - 1)
+    }
+  }
+
+  useEffect(() => {
+    if (count > 0) {
+      console.log('The count value changes to: ' + count)
+    }
+  }, [count])
+
   return (
     <div className={styles.container}>
-      <Button name="Adicionar" />
-      <span>0</span>
-      <Button name="Remover" />
+      <Button
+        name="Adicionar"
+        onClick={() => handleAdd()}
+        // onClick={() => show('Message from my custom hook')}
+      />
+      <span>{count}</span>
+      <Button name="Remover" onClick={() => handleRemove()} />
     </div>
   )
 }
